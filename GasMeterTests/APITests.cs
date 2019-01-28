d2177203e71aeefff2afe778dd37783e67fb9b81
Dawid Szczepaniak <dawid.szczepaniak@gmail.com>
2019-01-28 21:18:43 +0100

    Added tests and Measurement controller

---
+++ GasMeterTests/APITests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace GasMeterTests
+{
+    public class APITests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
